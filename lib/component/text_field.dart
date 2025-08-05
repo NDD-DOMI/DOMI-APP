9f4544e72e20fb9d26bdf959fc610ce8aa1a6041
최승우 <choi0250655@gmail.com>
2025-08-05 23:24:14 +0900

    login, font
    loginpage font

---
+++ lib/component/text_field.dart
@@ -0,0 +1,40 @@
+import 'package:domi_app/component/font.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextsField extends StatelessWidget {
+  const TextsField({
+    super.key,
+    required this.title,
+    required this.hintText,
+    required this.obsText,
+    this.suffixIcon,
+  });
+
+  final String title;
+  final String hintText;
+  final bool obsText;
+  final Widget? suffixIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    final basebroder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.r),
+      borderSide: const BorderSide(color: Color(0xff939393)),
+    );
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text( title, style: DomiTextStyle.rowTitle),
+        SizedBox(height: 4.h),
+        TextFormField(
+          obscureText: obsText,
+          style: TextStyle(decorationThickness: 0),
+          decoration: InputDecoration(hintText: hintText, border: basebroder,
+            suffixIcon: suffixIcon,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/view/login_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../component/login_button.dart';
+import '../component/text_field.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+class _LoginPageState extends State<LoginPage> {
+  bool obspwText= true;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('asset/images/domi.png',width: 100.w),
+              SizedBox(height: 12.h),
+              Image.asset('asset/images/title.png',),
+              SizedBox(height: 47.h),
+              TextsField(title: '아이디',hintText: '아이디를 입력', obsText: false, suffixIcon: null,),
+              SizedBox(height: 20.h),
+              TextsField(title: '비밀번호', hintText:'비밀번호를 입력', obsText: obspwText, suffixIcon: GestureDetector(
+                  onTap: ()=>setState(()=> obspwText=!obspwText),
+                  child: Builder(
+                      builder:(context) {
+                        if(obspwText){
+                          return Icon(Icons.visibility);
+                        }
+                        else {
+                          return Icon(Icons.visibility_off);
+                        }
+                      })),
+              ),
+              SizedBox(height: 197.h),
+              LoginBotton(title: '아이디'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
